8f4efe19654532f517de8b7dbed7a6116a5614fd
chasegraffeo <chasegraffeo@gmail.com>
2020-01-15 19:45:35 -0600

    second

--- tictac.js
+++ tictac.js
@@ -1,21 +1,36 @@
-let cell =document.querySelectorAll(`.cell`)
+let cell = document.querySelectorAll(`.cell`)
 
 cell.forEach(function(cell) {
 	cell.addEventListener(`click`, cellClicked);
 }); 
 
-let x_class = "X"
-let o_class = `o`
+let play1 = `X`
+let play2 = `O`
+let wngm = [
+	[1,2,3]
+	[4,5,6]
+	[7,8,9]
+	[1,4,7]
+	[2,5,8]
+	[3,6,9]
+	[3,5,7]
+	[1,5,9]
+]
 
 function cellClicked(e) {
 
-	e.target.textContent = x_class;
-	switch (x_class){
+	e.target.textContent = play1;
+	switch (play1){
 		case `X`: 
-			x_class = `O`
+			play1 = `O`
 			break;
 		case `O`:
-			x_class = `X`
+			play1 = `X`
 			break;
 	}
 } 
+// switch between player1 & 2
+// set x & o to block
+// define winning
+// define losing
+// define draw
